10f19405d55c09c0886f565a190d6c212c9a92d2
John Liu <tw00084811@gmail.com>
2014-07-18 16:43:24 +0800

    Modifying output of navigation log.

--- Client/main.cpp
+++ Client/main.cpp
@@ -730,7 +730,7 @@ int main(void)
 				     directionError);
 #else	
 			/* Record all data during navigation */
-			navlog.write("%02d,%02d,%+3.7f,%+3.7f,%+3.7f,%+3.7f,%+3.4f,%+3.4f,%3d,%3d\n",
+			navlog.write("%02d,%02d,%+3.7f,%+3.7f,%+3.7f,%+3.7f,%+3.4f,%+3.4f,%3.4f,%3d,%3d\n",
 				     path.getCurrentIndex()+1,	 // 1 -> current path number
 				     path.size(),                // 2 -> total path number
 				     latTarget,                  // 3 -> lat. of target
